8e7bf2ae45e47d6b62361070970dbb9e13df464c
Kunaaal13 <mrkunalyadav7@gmail.com>
2023-02-08 19:36:29 +0530

    Added gradient header

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className='bg-gradient-to-b from-gray-900 to-gray-600 min-h-screen w-full py-10'>
-      <h1 className='text-center text-3xl sm:text-4xl md:text-6xl font-extrabold'>
+      <h1 className='text-center text-3xl sm:text-4xl md:text-6xl text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100'>
         Game Of Thrones
       </h1>
 
